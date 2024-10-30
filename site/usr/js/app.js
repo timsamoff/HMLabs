e38fa6f979201574058402460b76bc4e461e01d2
Tim Samoff <samoff@gmail.com>
2024-10-30 14:19:31 -0700

    New v2.1.0 build

--- site/usr/js/app.js
+++ site/usr/js/app.js
@@ -1169,9 +1169,11 @@ $(document).ready(function()
 			console.log('System exited.');
 			$('.processing,.exit').remove();
 			Output('<p><span>- Last access log: </span></p>');
+			// Get key from GitHub Secrets
 			$.getJSON('hthttps://api.ipgeolocation.io/ipgeo?apiKey=GEOLOCATIONAPIKEY', function(data) {
 			$('.output').append('> ' + JSON.stringify(data, null, 2));
 			});
+			// Get key from GitHub Secrets
 			$.getJSON('https://api.ipgeolocation.io/ipgeo?apiKey=GEOLOCATIONAPIKEYf&fields=ip,country_name,state_prov,city,zipcode,latitude,longitude,time_zone')
 			.done(function(data) {
 				// Extract date and time from the timezone object
