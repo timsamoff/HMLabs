9c846dc87c94e8c356731ccb34e025d0c4f56b7d
Tim Samoff <samoff@gmail.com>
2024-10-30 14:15:20 -0700

    Buttoned everything up / switched to using private API Key

--- site/js/reset.js
+++ site/js/reset.js
@@ -1,3 +1,5 @@
-localStorage.removeItem('cdTime','firstTime','gameOver');
+localStorage.removeItem('cdTime');
+localStorage.removeItem('firstTime');
+localStorage.removeItem('gameOver');
 console.log('Cookies removed.');
 window.open ('index.html','_self',false);
